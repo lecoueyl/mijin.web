5b60e106d8e092b7645a46c8370d75b2e0807e4f
leo <leonard.lecouey@gmail.com>
2021-05-05 16:23:31 +0900

    feat(nav): add tailwindcss doc link

--- src/pages/docs/foundations/colors.vue
+++ src/pages/docs/foundations/colors.vue
@@ -87,7 +87,7 @@
 
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
       <div
-        v-for="name in ['blue', 'green', 'indigo', 'orange', 'pink', 'purple', 'red', 'teal', 'yellow']"
+        v-for="name in ['blue', 'green', 'indigo', 'orange', 'pink', 'purple', 'red', 'teal', 'yellow', 'cyan']"
         :key="name"
       >
         <MjHeadline

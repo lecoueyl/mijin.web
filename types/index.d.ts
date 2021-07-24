1f5a815f0ab973a89da7677d210e469efe265d48
leo <leonard.lecouey@gmail.com>
2021-07-24 17:43:54 +0900

    fix(highlightjs): add typescript declaration

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,4 +2,7 @@ declare module 'mijin';
 declare module 'mijin/src';
 declare module 'mijin/src/tailwind-preset';
 declare module 'vue-form';
+declare module 'highlight.js/lib/core';
+declare module 'highlight.js/lib/languages/javascript';
+declare module 'highlight.js/lib/languages/xml';
 declare module 'vue-highlightjs';

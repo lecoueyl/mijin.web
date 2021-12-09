53a6b85ba9a810706be0fc56040d9459e724b3eb
leo <leonard.lecouey@gmail.com>
2021-12-09 13:59:36 +0900

    feat(plugin): add extendIcons documentation

--- src/components/LayoutNavigation.vue
+++ src/components/LayoutNavigation.vue
@@ -67,7 +67,7 @@
             <a
               :href="href"
               :class="{
-                'text-gray-700 dark:text-gray-300 hover:text-gray-900 hover:dark:text-gray-100 ': !isActive,
+                'text-gray-700 dark:text-gray-300 hover:text-gray-900 dark:hover:text-gray-100 ': !isActive,
                 'text-gray-900 dark:text-gray-100 ': isActive,
               }"
               class="transition-colors duration-200 ease-in-out flex items-center"
@@ -101,21 +101,10 @@
             class="flex items-center"
           >
             <div class="h-6 w-6 p-1 mr-3 rounded-md inline-flex items-center justify-center bg-cyan-100">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke="currentColor"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+              <MjIcon
+                name="tailwindcss"
                 class="text-cyan-800"
-              >
-                <!-- eslint-disable-next-line max-len -->
-                <path d="M11.667 6c-2.49 0 -4.044 1.222 -4.667 3.667c.933 -1.223 2.023 -1.68 3.267 -1.375c.71 .174 1.217 .68 1.778 1.24c.916 .912 2 1.968 4.288 1.968c2.49 0 4.044 -1.222 4.667 -3.667c-.933 1.223 -2.023 1.68 -3.267 1.375c-.71 -.174 -1.217 -.68 -1.778 -1.24c-.916 -.912 -1.975 -1.968 -4.288 -1.968zm-4 6.5c-2.49 0 -4.044 1.222 -4.667 3.667c.933 -1.223 2.023 -1.68 3.267 -1.375c.71 .174 1.217 .68 1.778 1.24c.916 .912 1.975 1.968 4.288 1.968c2.49 0 4.044 -1.222 4.667 -3.667c-.933 1.223 -2.023 1.68 -3.267 1.375c-.71 -.174 -1.217 -.68 -1.778 -1.24c-.916 -.912 -1.975 -1.968 -4.288 -1.968z" />
-              </svg>
+              />
             </div>
             Tailwind CSS
           </a>
